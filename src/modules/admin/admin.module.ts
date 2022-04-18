7e98e4de98ae44b393fa3dceceab7db5b4c4ece0
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-04-18 10:55:26 +0000

    merge dev with master

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AdminController } from './admin.controller';
 import { User, UserSchema } from '../user/model/user.model';
-import { UserService } from './../user/user.service';
+import { UserService } from '../user/user.service';
 
 
 @Module({
